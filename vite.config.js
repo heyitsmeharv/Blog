75a579558b30c2772518bba8d69d73b27b8ddb45
Adam Harvey <heyitsmeharv@gmail.com>
2026-04-14 11:47:14 +0100

    fix: jsx-dev-runtime

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.(js|jsx)$/,
-  },
   optimizeDeps: {
     include: ["react-github-calendar"],
   },
